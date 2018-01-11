0f4c119445d577344e15e5c4f999516c46f5e85a
Esdras Fragoso da Sillva Neto <esdrasfragoso@guithub.com>
2018-01-11 19:16:15 -0300

    conclusão do objeto paciente.

--- TriagemEletronica/src/triagemeletronica/paciente/Paciente.java
+++ TriagemEletronica/src/triagemeletronica/paciente/Paciente.java
@@ -10,5 +10,170 @@ package triagemeletronica.paciente;
  * @author Esdras Fragoso
  */
 public class Paciente {
+    private String nome;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String cartao_sus;
+    private String rg;
+    private String procedencia;
+    private String motivo_da_vinda;
+    private String zona;
+    private boolean dores;
+    private float temperatura;
+    private float sistole;
+    private float diastola;
+    private boolean diabetico;
+    private float resultado_test_clicemia;
+    private boolean alergia;
+    private String resultado_alergia;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCartao_sus() {
+        return cartao_sus;
+    }
+
+    public void setCartao_sus(String cartao_sus) {
+        this.cartao_sus = cartao_sus;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getProcedencia() {
+        return procedencia;
+    }
+
+    public void setProcedencia(String procedencia) {
+        this.procedencia = procedencia;
+    }
+
+    public String getMotivo_da_vinda() {
+        return motivo_da_vinda;
+    }
+
+    public void setMotivo_da_vinda(String motivo_da_vinda) {
+        this.motivo_da_vinda = motivo_da_vinda;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public boolean isDores() {
+        return dores;
+    }
+
+    public void setDores(boolean dores) {
+        this.dores = dores;
+    }
+
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public float getSistole() {
+        return sistole;
+    }
+
+    public void setSistole(float sistole) {
+        this.sistole = sistole;
+    }
+
+    public float getDiastola() {
+        return diastola;
+    }
+
+    public void setDiastola(float diastola) {
+        this.diastola = diastola;
+    }
+
+    public boolean isDiabetico() {
+        return diabetico;
+    }
+
+    public void setDiabetico(boolean diabetico) {
+        this.diabetico = diabetico;
+    }
+
+    public float getResultado_test_clicemia() {
+        return resultado_test_clicemia;
+    }
+
+    public void setResultado_test_clicemia(float resultado_test_clicemia) {
+        this.resultado_test_clicemia = resultado_test_clicemia;
+    }
+
+    public boolean isAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(boolean alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getResultado_alergia() {
+        return resultado_alergia;
+    }
+
+    public void setResultado_alergia(String resultado_alergia) {
+        this.resultado_alergia = resultado_alergia;
+    }
+
+    public Paciente() {
+    }
     
 }
